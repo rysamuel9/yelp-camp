e97df28fa6f9d655a0618e8a5914a83848ec3930
Rony Samuel Tamba <ronysamuel19@gmail.com>
2022-06-07 19:54:33 +0700

    joi validation

--- index.js
+++ index.js
@@ -55,6 +55,9 @@ app.post(
       campground: Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
+        imgae: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
       }).required(),
     });
     const { error } = campgroundSchema.validate(req.body);
