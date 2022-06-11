de6edb17975af9dd7d46cad3845d0a6af0c3cb58
Rony Samuel Tamba <ronysamuel19@gmail.com>
2022-06-11 21:51:22 +0700

    delete reviews

--- index.js
+++ index.js
@@ -132,6 +132,16 @@ app.post(
   }),
 );
 
+app.delete(
+  '/campgrounds/:id/reviews/:reviewId',
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  }),
+);
+
 app.all('*', (req, res, next) => {
   // res.render('campgrounds/error');
   next(new ExpressError('PAGE NOT FOUND!', 404));
